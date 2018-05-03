98c079c9a6d59478e54dfdec1896bbd89b124e11
ivaris <ivaris81@outlook.com>
2018-05-02 23:28:25 -0400

    add api resources implementation

---
+++ ap-service/main/java/com/ivar/enterprise/ap/domain/AirportFrequency.java
@@ -0,0 +1,67 @@
+package com.ivar.enterprise.ap.domain;
+
+public class AirportFrequency {
+    private Object _id;
+    private long id;
+    private String airportRef;
+    private String airportIdent;
+    private String type;
+    private String description;
+    private Double frequency;
+
+    public Object get_id() {
+        return _id;
+    }
+
+    public void set_id(Object _id) {
+        this._id = _id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAirportRef() {
+        return airportRef;
+    }
+
+    public void setAirportRef(String airportRef) {
+        this.airportRef = airportRef;
+    }
+
+    public String getAirportIdent() {
+        return airportIdent;
+    }
+
+    public void setAirportIdent(String airportIdent) {
+        this.airportIdent = airportIdent;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(Double frequency) {
+        this.frequency = frequency;
+    }
+}
