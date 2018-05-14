76cb5ba90dcf6af2810282f1ba6763e51f6759be
Ravi Vavilipalli <rvavilipalli@Ravis-MacBook.local>
2018-05-14 08:33:06 -0400

    changes to search results

--- ap-web/src/app/components/searchresult.component.ts
+++ ap-web/src/app/components/searchresult.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { Airport } from '../domains/airport';
 import { AirportService }  from '../providers/airport.service';
+import { Country } from '../domains/country';
+import { Region } from '../domains/region';
 
 @Component({
   selector: 'search-result',
@@ -11,7 +13,8 @@ import { AirportService }  from '../providers/airport.service';
 })
 export class SearchResultComponent {
     airports: Airport[];
-
+    @Input() country: Country;
+    @Input() region: Region;
     constructor(private airportService: AirportService) { }
 
 }
